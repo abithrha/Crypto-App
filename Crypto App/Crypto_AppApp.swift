cccfd2e2e3b8be6caf34644d351adf0f2afa103c
Abith <abithrha@gmail.com>
2024-05-26 00:25:23 +0530

    Initial Commit

---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by Abith Rajakumaran on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
